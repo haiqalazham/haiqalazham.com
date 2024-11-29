9469563fba6608ddb743e5e891fc8479ee57e7b8
Haiqal Azham <haiqal@haiqalazham.com>
2024-11-29 16:53:17 +0800

    Simplify to coming soon page for publish

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,13 +52,9 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-          <Navbar />
+      <body className="antialiased">
+        <main className="flex items-center justify-center h-screen">
           {children}
-          <Footer />
-          <Analytics />
-          <SpeedInsights />
         </main>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,12 @@
-import { BlogPosts } from 'app/components/posts'
-
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+      <h1 className="text-2xl font-semibold">
         Haiqal Azham
       </h1>
-      <p className="mb-4">
-        {`Hi, I’m Haiqal Akmal. I’m a technology enthusiast and an avid photographer. I specialize in street and landscape photography.`}
+      <p>
+        coming soon
       </p>
-      <p className="mb-4">
-        {`I’m currently studying Mechanical Engineering at the University of Michigan. Other hobbies include programming, gaming and finding ways to accelerate the adoption of renewable energy.`}
-      </p>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
     </section>
   )
 }
