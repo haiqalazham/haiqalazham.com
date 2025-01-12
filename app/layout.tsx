9ccf5654349a037e2e3d397f67c06cf8b28ebc1c
Haiqal Azham <haiqal@haiqalazham.com>
2025-01-12 13:35:00 +0800

    Updated header, footer, sitemap and RSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,15 +11,15 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Haiqal Azham',
+    template: '%s | Haiqal Azham',
   },
   description: 'This is my portfolio.',
   openGraph: {
-    title: 'My Portfolio',
+    title: 'Haiqal Azham',
     description: 'This is my portfolio.',
     url: baseUrl,
-    siteName: 'My Portfolio',
+    siteName: 'Haiqal Azham',
     locale: 'en_US',
     type: 'website',
   },
