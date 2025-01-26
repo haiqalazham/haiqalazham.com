ebcbadd74077cbb340aaf009937b8e81006e1933
Haiqal Azham <haiqal@haiqalazham.com>
2025-01-26 19:06:59 +0800

    Publish first blog post

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,11 @@ export default function Page() {
   return (
     <section>
       <p className="mb-4">Hello! I'm Haiqal, a technology enthusiast and an avid photographer. I'm interested in human-computer interaction, walkable cities and tools for thought.</p>
-      <p>I'm currently working on implementing SAP S4/HANA in PETRONAS. In my free time, I like to read books, tend my digital garden, and watch movies.</p>
-      {/*
+      <p>I'm currently working on ████████. In my free time, I like to read books, tend my digital garden, and watch movies.</p>
       <div className="my-8">
         <h2 className="font-semibold text-xl mb-8 tracking-tighter">My Writings</h2>
         <BlogPosts />
       </div>
-      */}
     </section>
   )
 }
