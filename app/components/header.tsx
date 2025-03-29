3a8b430f4ca599d8ece43b4639b5b07e06c10aa4
Haiqal Azham <85307334+haiqalazham@users.noreply.github.com>
2025-03-29 17:44:06 +0800

    Add construction page

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -14,13 +14,23 @@ const navItems = {
 
 export default function Header() {
   return (
-    <header className="py-6">
+    <header className="py-6 flex justify-between">
       <div>
         <Link href="/" className="font-bold tracking-tight">
           Haiqal Azham ◡̈
         </Link>
       </div>
-      <nav></nav>
+      <nav>
+        <div className="flex flex-row">
+          {Object.entries(navItems).map(([path, { name }]) => {
+            return (
+              <Link key={path} href={path} className="px-2">
+                {name}
+              </Link>
+            );
+          })}
+        </div>
+      </nav>
     </header>
   );
 }
---
+++ app/gallery/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <section className="py-8">
+      <h1 className="text-xl font-bold mb-4">🚧 My Gallery 🚧</h1>
+      <p className="mb-4">This page is under construction. Coming soon!</p>
+    </section>
+  );
+}
