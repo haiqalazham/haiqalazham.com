9865a5fe2081aae53573c3f08100251ae7cff115
Haiqal Azham <85307334+haiqalazham@users.noreply.github.com>
2025-04-03 17:08:44 +0800

    Update MDX layout

--- app/now/layout.tsx
+++ app/now/layout.tsx
@@ -1,4 +1,6 @@
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   // Create any shared layout or styles here
-  return <div className="prose">{children}</div>;
+  return <div className="prose prose-zinc">{children}</div>;
 }
+
+// TODO: text color and heading size
--- app/uses/layout.tsx
+++ app/uses/layout.tsx
@@ -1,4 +1,6 @@
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   // Create any shared layout or styles here
-  return <div className="prose">{children}</div>;
+  return <div className="prose prose-stone">{children}</div>;
 }
+
+// TODO: text color and heading size
